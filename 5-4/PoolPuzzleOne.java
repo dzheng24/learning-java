479859f745f6486b1e3d6a25347668df9c6ee17f
David Zheng <zhengdav@msu.edu>
2020-05-04 17:16:02 -0700

    initial try

---
+++ 5-4/PoolPuzzleOne.java
@@ -0,0 +1,25 @@
+class PoolPuzzleOne {
+  public static void main(String [] args) {
+    int x = 0;
+    
+    while (x < 4) {
+      System.out.print("a");
+      if (x < 1) {
+        System.out.print(" ");
+      }
+      System.out.print("noise");
+      if (x > 1) {
+      System.out.print("noys");
+      x = x + 2;
+      }
+      if (x == 1) {
+        System.out.print("oise");
+      }
+      if (x > 3) {
+        System.out.print(" oyster");
+      }
+      System.out.println("");
+      x = x + 1;
+    }
+  }
+}
